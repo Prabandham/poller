0c48c86d9875cfd10d3c8a5c127cf01f936d6b41
Srinidhi Prabandham <srinidhi.prabandham@icicletech.com>
2018-03-14 17:27:22 +0530

    [New Poll] backend for submitting a poll first cut

--- lib/poller/questions/poll_question.ex
+++ lib/poller/questions/poll_question.ex
@@ -24,6 +24,33 @@ defmodule Poller.Questions.PollQuestion do
     |> unique_constraint(:content)
   end
 
-  def create_poll_with_answers(_question, _answers) do
+  def create_poll_with_answers(question, answers, user_id, tags \\ []) do
+    question_params = %{content: question, user_id: user_id, tags: tags}
+    answer_params = Enum.map(answers, fn(answer) ->
+      %{content: answer}
+    end)
+    Repo.transaction(fn ->
+      changeset = %PollQuestion{} |> PollQuestion.changeset(question_params)
+      question = case Repo.insert(changeset) do
+        {:ok, question} -> question
+        {:error, _changeset} -> nil
+      end
+
+      unless question == nil do
+        Enum.map(answer_params, fn(params) ->
+          changeset = question
+                      |> build_assoc(:answers)
+                      |> Answer.changeset(params)
+          case Repo.insert(changeset) do
+            {:ok, _answer} ->
+              question_with_answers = question |> Poller.Repo.preload(:user, answers: [:user_votes])
+              {:ok, question_with_answers}
+            {:error, changeset} ->
+              Repo.rollback(:answer)
+              {:error, "Could not create Question"}
+          end
+        end)
+      end
+    end)
   end
 end
--- lib/poller_web/controllers/poll_controller.ex
+++ lib/poller_web/controllers/poll_controller.ex
@@ -3,9 +3,15 @@ defmodule PollerWeb.PollController do
 
   use PollerWeb, :controller
 
-  def create_poll(conn, _params) do
+  def create_poll(conn, params) do
     data = %{status: "error", data: "Could not create Poll !"}
-
+    question = params["question"]
+    answers = params["answers"]
+    user_id = params["user_id"]
+    case Poller.Questions.PollQuestion.create_poll_with_answers(question, answers, user_id) do
+      {:ok, question} -> #TODO broadcast to all users the new poll and send an Ok to the fontend so that the modal can close.
+      {:error, _} -> #TODO send the necessary error to the user so that he can edit the question / answer and then submit again.
+    end
     conn
     |> render("create_poll.json", data: data)
   end
